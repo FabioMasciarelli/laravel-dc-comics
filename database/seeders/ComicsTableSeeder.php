296138637e71055336b8e27f1802724faf47614a
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-06-14 15:44:28 +0200

    add: model and seeder for comics table

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+
+            $newComic = new Comic();
+            $newComic->fill($comic);
+            $newComic->save();
+        }
+    }
+}
